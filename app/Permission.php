354b5699d7222bf0ad5bcce27389c36a5a1ee754
Admin <Admin>
2020-08-21 09:22:05 +0700

    iugheiurgneiurgn

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
